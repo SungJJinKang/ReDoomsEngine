b8ffb87584abbd89c149d31bfe610ad42ca46455
SungJinKang <hour30000@gmail.com>
2024-01-01 19:33:01 +0900

    add some comments
    Former-commit-id: 97e1e0d0ac32033fc122f2517e3b48c1e590026a

--- ReDoomsEngine/Source/Runtime/D3D12/D3D12RootSignature.h
+++ ReDoomsEngine/Source/Runtime/D3D12/D3D12RootSignature.h
@@ -1,4 +1,6 @@
 #pragma once
+
+// ref : https://learn.microsoft.com/en-us/windows/win32/direct3d12/creating-a-root-signature, UE5 FD3D12RootSignatureDesc::FD3D12RootSignatureDesc
 class FD3D12RootSignature
 {
 };
