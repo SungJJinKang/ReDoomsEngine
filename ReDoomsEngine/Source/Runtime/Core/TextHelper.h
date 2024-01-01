130520a56b35efb6f56e962b9ce2473dc41194c7
SungJinKang <hour30000@gmail.com>
2024-01-01 19:06:36 +0900

    add some comments, remove some unused codes
    Former-commit-id: 9adcc576f8a2a0929e3488ad63996e132df71924

--- ReDoomsEngine/Source/Runtime/Core/TextHelper.h
+++ ReDoomsEngine/Source/Runtime/Core/TextHelper.h
@@ -2,5 +2,6 @@
 #include "EAStdC/EAString.h"
 #include "EASTL/string.h"
 
+// Don't store returned address in char(wchar) pointer. The address points to garbage data 
 #define UTF8_TO_WCHAR(Str) EA::StdC::ConvertString<eastl::u8string_view, eastl::wstring>(Str).c_str()
 #define ANSI_TO_WCHAR(Str) UTF8_TO_WCHAR(Str)
