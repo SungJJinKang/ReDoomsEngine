d6616d0def77eaa0a3e4246cce5908404bc9a3e4
SungJinKang <hour30000@gmail.com>
2024-02-16 01:49:30 +0900

    change some dynamic_cast to static_cast. this was slow when profiled

--- ReDoomsEngine/Source/Runtime/D3D12/D3D12Shader.cpp
+++ ReDoomsEngine/Source/Runtime/D3D12/D3D12Shader.cpp
@@ -492,13 +492,13 @@ void FShaderParameterContainerTemplate::ApplyShaderParameters(FD3D12CommandConte
 	{
 		if (ShaderParamter->IsConstantBuffer())
 		{
-			FShaderParameterConstantBuffer* ShaderParameterConstantBuffer = dynamic_cast<FShaderParameterConstantBuffer*>(ShaderParamter);
+			FShaderParameterConstantBuffer* ShaderParameterConstantBuffer = static_cast<FShaderParameterConstantBuffer*>(ShaderParamter);
 			EA_ASSERT(ShaderParameterConstantBuffer);
 			ConstantBufferBindPointInfoList[ShaderParameterConstantBuffer->GetConstantBufferReflectionData()->ResourceBindingDesc.BindPoint] = ShaderParameterConstantBuffer;
 		}
 		else if (ShaderParamter->IsSRV())
 		{
-			FShaderParameterShaderResourceView* ShaderParameterSRV = dynamic_cast<FShaderParameterShaderResourceView*>(ShaderParamter);
+			FShaderParameterShaderResourceView* ShaderParameterSRV = static_cast<FShaderParameterShaderResourceView*>(ShaderParamter);
 			EA_ASSERT(ShaderParameterSRV);
 			SRVBindPointInfoList[ShaderParameterSRV->GetReflectionData().BindPoint] = ShaderParameterSRV->GetTargetSRV();
 		}
