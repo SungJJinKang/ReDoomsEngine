9f0c2c9d41083f33cdebf3caad5b9c9c7124281d
SungJinKang <hour30000@gmail.com>
2024-07-27 17:04:40 +0900

    imp...
    Former-commit-id: 05e70865bb53ede5d518a77bd0873a233ed2ea69

---
+++ ReDoomsEngine/Assets/Shader/SceneData.hlsl
@@ -0,0 +1,16 @@
+﻿
+struct FPrimitiveSceneData
+{
+	uint		Flags;
+	uint		VisibilityFlags;
+	float4x4	LocalToWorld;
+	float4x4	WorldToLocal;
+};
+
+StructuredBuffer<float4> PrimitiveSceneData;
+
+FPrimitiveSceneData GetPrimitiveIdSceneData(uint PrimitiveId)
+{
+	FPrimitiveSceneData SceneData = (FPrimitiveSceneData)0;
+	return SceneData;
+}
