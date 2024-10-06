74413b71679cfc86a3bbf5a21cab0e05aa769299
SungJinKang <hour30000@gmail.com>
2024-10-07 00:13:32 +0900

    move cubemap util functions to new file

---
+++ ReDoomsEngine/Assets/Shader/CubemapUtils.hlsl
@@ -0,0 +1,28 @@
+﻿#ifndef __CUBEMAPUTILS_HLSLI__
+#define __CUBEMAPUTILS_HLSLI__
+
+float3 GetCubemapSamplingVector(float2 InScreenPos, uint InCubemapFaceIndex, float2 InCubemapSize)
+{
+    float2 st = InScreenPos/InCubemapSize;
+    float2 uv = 2.0 * float2(st.x, 1.0-st.y) - float2(1.0f, 1.0f);
+
+    float3 ret;
+    if(InCubemapFaceIndex == 0)      ret = float3(1.0,  uv.y, -uv.x); // right
+    else if(InCubemapFaceIndex == 1) ret = float3(-1.0, uv.y,  uv.x); // left
+    else if(InCubemapFaceIndex == 2) ret = float3(uv.x, 1.0, -uv.y); // up
+    else if(InCubemapFaceIndex == 3) ret = float3(uv.x, -1.0, uv.y); // down
+    else if(InCubemapFaceIndex == 4) ret = float3(uv.x, uv.y, 1.0); // front
+    else if(InCubemapFaceIndex == 5) ret = float3(-uv.x, uv.y, -1.0); // back
+    return normalize(ret);
+}
+
+float2 ConvertCubemapSamplingVectorToEquirectangularTexCoord(float3 CubemapSamplingVector)
+{
+	float u = atan2(CubemapSamplingVector.x, CubemapSamplingVector.z) / (2*PI) + 0.5;
+	float v = CubemapSamplingVector.y * 0.5 + 0.5;
+	v = -v + 1.0f;
+	
+	return float2(u, v);
+}
+
+#endif
--- ReDoomsEngine/Assets/Shader/RenderCubemap.hlsl
+++ ReDoomsEngine/Assets/Shader/RenderCubemap.hlsl
@@ -3,6 +3,7 @@
 #include "Common.hlsl"
 
 #include "ScreenDrawCommon.hlsl"
+#include "CubemapUtils.hlsl"
 
 Texture2D<float3> HDREnvMapTexture;
 int CubemapFaceIndex;
@@ -21,31 +22,12 @@ ScreenDrawPSInput RenderCubemapVS(
 	return Result;
 }
 
-float3 GetCubemapSamplingVector(float2 InScreenPos)
-{
-    float2 st = InScreenPos/CubemapSize;
-    float2 uv = 2.0 * float2(st.x, 1.0-st.y) - float2(1.0f, 1.0f);
-
-    float3 ret;
-    if(CubemapFaceIndex == 0)      ret = float3(1.0,  uv.y, -uv.x); // right
-    else if(CubemapFaceIndex == 1) ret = float3(-1.0, uv.y,  uv.x); // left
-    else if(CubemapFaceIndex == 2) ret = float3(uv.x, 1.0, -uv.y); // up
-    else if(CubemapFaceIndex == 3) ret = float3(uv.x, -1.0, uv.y); // down
-    else if(CubemapFaceIndex == 4) ret = float3(uv.x, uv.y, 1.0); // front
-    else if(CubemapFaceIndex == 5) ret = float3(-uv.x, uv.y, -1.0); // back
-    return normalize(ret);
-}
 
 void RenderCubemapPS(
     ScreenDrawPSInput Input, 
     out float4 Output : SV_Target0
 )
 {
-	float3 CubemapSamplingVector = GetCubemapSamplingVector(Input.ScreenPosition);
-
-	float u = atan2(CubemapSamplingVector.x, CubemapSamplingVector.z) / (2*PI) + 0.5;
-	float v = CubemapSamplingVector.y * 0.5 + 0.5;
-	v = -v + 1.0f;
-
-	Output = float4(HDREnvMapTexture.Sample(StaticPointClampSampler, float2(u, v)).xyz, 1.0f);
+	float3 CubemapSamplingVector = GetCubemapSamplingVector(Input.ScreenPosition, CubemapFaceIndex, CubemapSize);
+	Output = float4(HDREnvMapTexture.Sample(StaticPointClampSampler, ConvertCubemapSamplingVectorToEquirectangularTexCoord(CubemapSamplingVector)).xyz, 1.0f);
 }
