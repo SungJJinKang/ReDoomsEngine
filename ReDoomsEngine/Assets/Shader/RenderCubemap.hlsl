398ad84f2f7845e0c8a19053d6a94effd75d0f78
SungJinKang <hour30000@gmail.com>
2024-10-07 00:08:13 +0900

    fixup converting cubemap vector to equirectangular texture coordinate

--- ReDoomsEngine/Assets/Shader/RenderCubemap.hlsl
+++ ReDoomsEngine/Assets/Shader/RenderCubemap.hlsl
@@ -27,12 +27,12 @@ float3 GetCubemapSamplingVector(float2 InScreenPos)
     float2 uv = 2.0 * float2(st.x, 1.0-st.y) - float2(1.0f, 1.0f);
 
     float3 ret;
-    if(CubemapFaceIndex == 0)      ret = float3(1.0,  uv.y, -uv.x);
-    else if(CubemapFaceIndex == 1) ret = float3(-1.0, uv.y,  uv.x);
-    else if(CubemapFaceIndex == 2) ret = float3(uv.x, 1.0, -uv.y);
-    else if(CubemapFaceIndex == 3) ret = float3(uv.x, -1.0, uv.y);
-    else if(CubemapFaceIndex == 4) ret = float3(uv.x, uv.y, 1.0);
-    else if(CubemapFaceIndex == 5) ret = float3(-uv.x, uv.y, -1.0);
+    if(CubemapFaceIndex == 0)      ret = float3(1.0,  uv.y, -uv.x); // right
+    else if(CubemapFaceIndex == 1) ret = float3(-1.0, uv.y,  uv.x); // left
+    else if(CubemapFaceIndex == 2) ret = float3(uv.x, 1.0, -uv.y); // up
+    else if(CubemapFaceIndex == 3) ret = float3(uv.x, -1.0, uv.y); // down
+    else if(CubemapFaceIndex == 4) ret = float3(uv.x, uv.y, 1.0); // front
+    else if(CubemapFaceIndex == 5) ret = float3(-uv.x, uv.y, -1.0); // back
     return normalize(ret);
 }
 
@@ -42,9 +42,10 @@ void RenderCubemapPS(
 )
 {
 	float3 CubemapSamplingVector = GetCubemapSamplingVector(Input.ScreenPosition);
-	float phi   = atan2(CubemapSamplingVector.z, CubemapSamplingVector.x);
-	float theta = acos(CubemapSamplingVector.y);
-	float2 EquirectangularTexCoord = float2(phi/(PI*2), theta/PI);
 
-	Output = float4(HDREnvMapTexture.Sample(StaticPointClampSampler, EquirectangularTexCoord).xyz, 1.0f);
+	float u = atan2(CubemapSamplingVector.x, CubemapSamplingVector.z) / (2*PI) + 0.5;
+	float v = CubemapSamplingVector.y * 0.5 + 0.5;
+	v = -v + 1.0f;
+
+	Output = float4(HDREnvMapTexture.Sample(StaticPointClampSampler, float2(u, v)).xyz, 1.0f);
 }
